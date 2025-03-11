55ec1c54160b0b1645cc351dbe3ef460f646237f
dayuan.jiang <jdy.toh@gmail.com>
2025-03-11 23:44:27 +0900

    feat: update project details and remove outdated entries

--- app/data.ts
+++ app/data.ts
@@ -29,21 +29,13 @@ type SocialLink = {
 
 export const PROJECTS: Project[] = [
   {
-    name: 'Motion Primitives Pro',
+    name: 'Next Grammar Checker',
     description:
-      'Advanced components and templates to craft beautiful websites.',
-    link: 'https://pro.motion-primitives.com/',
+      'LLM based Grammar checker with a diff UI.',
+    link: 'https://next-grammar-checker.vercel.app/',
     video:
       'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/newProfileItem/d898be8a-7037-4c71-af0c-8997239b050d.mp4?_a=DATAdtAAZAA0',
     id: 'project1',
-  },
-  {
-    name: 'Motion Primitives',
-    description: 'UI kit to make beautiful, animated interfaces.',
-    link: 'https://motion-primitives.com/',
-    video:
-      'https://res.cloudinary.com/read-cv/video/upload/t_v_b/v1/1/profileItems/W2azTw5BVbMXfj7F53G92hMVIn32/XSfIvT7BUWbPRXhrbLed/ee6871c9-8400-49d2-8be9-e32675eabf7e.mp4?_a=DATAdtAAZAA0',
-    id: 'project2',
   },
 ]
 
@@ -56,22 +48,7 @@ export const WORK_EXPERIENCE: WorkExperience[] = [
     link: 'https://ibelick.com',
     id: 'work1',
   },
-  {
-    company: 'Freelance',
-    title: 'Design Engineer',
-    start: '2022',
-    end: '2024',
-    link: 'https://ibelick.com',
-    id: 'work2',
-  },
-  {
-    company: 'Freelance',
-    title: 'Front-end Developer',
-    start: '2017',
-    end: 'Present',
-    link: 'https://ibelick.com',
-    id: 'work3',
-  },
+
 ]
 
 export const BLOG_POSTS: BlogPost[] = [
